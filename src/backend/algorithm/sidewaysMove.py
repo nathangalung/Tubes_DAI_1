f00f3b0893a37e8c1c2561ad605d183acbc81937
tamaramyrn <tamaramyrn@gmail.com>
2024-11-10 16:20:19 +0700

    update sideways move

--- src/backend/algorithm/sidewaysMove.py
+++ src/backend/algorithm/sidewaysMove.py
@@ -3,18 +3,13 @@ from typing import Tuple, List
 from . import utils
 
 def swap_elements(cube: List[List[List[int]]], pos1: Tuple[int, int, int], pos2: Tuple[int, int, int], calculate_cost: bool = False) -> int:
+    """Swaps elements in the cube at the given positions pos1 and pos2."""
     i, j, k = pos1
     l, m, n = pos2
     cube[i][j][k], cube[l][m][n] = cube[l][m][n], cube[i][j][k]
 
-    if calculate_cost:
-        cost = utils.objective_function(cube)
-        cube[i][j][k], cube[l][m][n] = cube[l][m][n], cube[i][j][k]
-        return cost
-
-    return None
-
 def find_best_neighbor(cube: List[List[List[int]]], best_cost: int, max_sideways: int, sideways: int) -> Tuple[int, Tuple[int, int, int, int, int, int]]:
+    """Finds the best neighboring configuration of the cube by trying all possible swaps."""
     N = len(cube)
     best_neighbor_cost = best_cost
     best_swap = (-1, -1, -1, -1, -1, -1)
@@ -22,51 +17,49 @@ def find_best_neighbor(cube: List[List[List[int]]], best_cost: int, max_sideways
     for i in range(N):
         for j in range(N):
             for k in range(N):
-                current_cost = utils.objective_function(cube)
-                
                 for l in range(i, N):  
                     for m in (range(j, N) if i == l else range(N)):
                         for n in (range(k, N) if i == l and j == m else range(N)):
                             if (i == l and j == m and k == n):
-                                continue
-                            
-                            swap_elements(cube, (i, j, k), (l, m, n))
-                            new_cost = utils.objective_function(cube) + utils.objective_function(cube)
-                            total_cost = best_cost - current_cost + new_cost
+                                continue 
 
-                            if total_cost < best_neighbor_cost or (
-                                total_cost == best_neighbor_cost and sideways < max_sideways
-                            ):
-                                best_neighbor_cost = total_cost
-                                best_swap = (i, j, k, l, m, n)
+                            swap_elements(cube, (i, j, k), (l, m, n))
+                            new_cost = utils.objective_function(cube)
+
+                            if new_cost < best_neighbor_cost or (new_cost == best_neighbor_cost and sideways < max_sideways):
+                                best_neighbor_cost = new_cost 
+                                best_swap = (i, j, k, l, m, n) 
 
                             swap_elements(cube, (i, j, k), (l, m, n))
 
     return best_neighbor_cost, best_swap
 
-def sideways_move_algorithm(cube: List[List[List[int]]], max_sideways: int = 100, max_iterations: int = 1000) -> Tuple[List[List[List[int]]], int, List[int], float]:
+def sideways_move_algorithm(cube: List[List[List[int]]], max_sideways: int = 30) -> dict:
+    """Performs the sideways move algorithm to optimize the cube configuration."""
     current_cost = utils.objective_function(cube)
-    best_cost = current_cost
-    iterations, sideways = 0, 0
-    costs = []
+    best_cost = current_cost 
+    iterations, sideways = 0, 0 
+    costs = []  
 
-    start_time = time.time()
+    start_time = time.time() 
 
-    while iterations < max_iterations:
+    while True:
         best_neighbor_cost, best_swap = find_best_neighbor(cube, best_cost, max_sideways, sideways)
 
-        if best_neighbor_cost >= best_cost and sideways >= max_sideways:
+        if sideways >= max_sideways:
             break
-
-        if best_neighbor_cost == best_cost:
-            sideways += 1
-        else:
-            sideways = 0
 
         if best_swap != (-1, -1, -1, -1, -1, -1):
             i, j, k, l, m, n = best_swap
-            swap_elements(cube, (i, j, k), (l, m, n))
-            best_cost = best_neighbor_cost
+            swap_elements(cube, (i, j, k), (l, m, n)) 
+
+            current_cost = best_neighbor_cost
+
+            if current_cost < best_cost:
+                best_cost = current_cost 
+                sideways = 0 
+            elif current_cost == best_cost:
+                sideways += 1
 
         iterations += 1
         costs.append(best_cost)
@@ -76,7 +69,7 @@ def sideways_move_algorithm(cube: List[List[List[int]]], max_sideways: int = 100
     return {
         "final_cube": cube,
         "final_cost": best_cost,
-        "duration": round(duration, 2),
-        "iterations": len(costs),
-        "costs": costs
+        "duration": round(duration, 2), 
+        "iterations": iterations, 
+        "costs": costs 
     }
