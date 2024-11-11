80cd6fecf6f4e486a12b4d2f8f3888befd7371fe
YusrilMahendraa <yusrilfazri14@gmail.com>
2024-11-11 16:07:54 +0700

    update comment random restart

--- src/backend/algorithm/randomRestart.py
+++ src/backend/algorithm/randomRestart.py
@@ -5,25 +5,25 @@ from typing import List, Tuple
 from . import utils
 
 def swap_elements(cube: List[List[List[int]]], pos1: Tuple[int, int, int], pos2: Tuple[int, int, int]) -> None:
-    """Menukar elemen dalam kubus pada posisi yang diberikan."""
+    #menukar elemen 
     i, j, k = pos1
     l, m, n = pos2
     cube[i][j][k], cube[l][m][n] = cube[l][m][n], cube[i][j][k]
 
 def generate_random_neighbor(cube: List[List[List[int]]]) -> List[List[List[int]]]:
-    """Menghasilkan tetangga acak dengan menukar dua elemen acak dalam kubus."""
+    #menghasilkan anak secara acak dengan menukar 2 elemen
     N = len(cube)
     i1, j1, k1 = random.randint(0, N-1), random.randint(0, N-1), random.randint(0, N-1)
     i2, j2, k2 = random.randint(0, N-1), random.randint(0, N-1), random.randint(0, N-1)
     
-    # Pastikan kedua posisi berbeda
+    #untuk memastikan pertukarannya gasama
     while (i1, j1, k1) == (i2, j2, k2):
         i2, j2, k2 = random.randint(0, N-1), random.randint(0, N-1), random.randint(0, N-1)
     
     # Salin kubus untuk tetangga baru
     new_cube = [[[cube[i][j][k] for k in range(N)] for j in range(N)] for i in range(N)]
     
-    # Tukar elemen
+    # tukar elemen
     swap_elements(new_cube, (i1, j1, k1), (i2, j2, k2))
     
     return new_cube
@@ -49,7 +49,7 @@ def random_restart_algorithm(cube: List[List[List[int]]], max_restart: int = 10,
     while restart < max_restart:
         iteration = 0
         while iteration < max_iterations:
-            # Generate random neighbor
+            # melakukan Generate random neighbor
             neighbor_cube = generate_random_neighbor(current_cube)
             neighbor_cost = utils.objective_function(neighbor_cube)
             
@@ -80,7 +80,7 @@ def random_restart_algorithm(cube: List[List[List[int]]], max_restart: int = 10,
         iteration_restart.append(iteration)
         restart += 1
 
-        # Restart pencarian dengan solusi acak baru
+        # Restart pencarian dengan solusi acak yang baru
         current_cube = utils.initialize_random_cube(N)
         current_cost = utils.objective_function(current_cube)
 
